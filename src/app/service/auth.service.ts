04d0cc81e0829ae4a0ebc8be638feca13be62b7c
Wendell Gomes <w3ndell@hotmail.com>
2021-03-23 13:57:19 -0300

    Consumo entre back e front - Cadastrar/Login

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,9 +14,9 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/ususarios/logar', userLogin)
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
   }
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/ususarios/cadastrar', User)
+    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
 }
