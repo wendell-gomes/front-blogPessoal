3fdc0eeaa43869a30f4364bfa82e05426911b442
Wendell Gomes <w3ndell@hotmail.com>
2021-03-23 12:40:24 -0300

    Consumo entre back e front

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/User';
+import { UserLogin } from '../models/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin>('http://localhost:8080/ususarios/logar', userLogin)
+  }
+  cadastrar(user: User): Observable<User>{
+    return this.http.post<User>('http://localhost:8080/ususarios/cadastrar', User)
+  }
+}
