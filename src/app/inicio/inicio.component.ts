8b7bd1aa7d110fd8301bf903dd7dd4972f8443a5
Wendell Gomes <w3ndell@hotmail.com>
2021-04-01 13:24:19 -0300

    Iniciando post/get de postagens

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../models/Postagem';
+import { Tema } from '../models/Tema';
+import { User } from '../models/User';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,14 +14,63 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  tema: Tema = new Tema
+  listaTemas: Tema[]
+  idTema: number
+
+
+  user: User = new User
+  idUser = environment.id
+
+  listaPostagens: Postagem[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private TemaService: TemaService
   ) { }
 
   ngOnInit() {
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+
+    this.getAllTemas()
+    this.getAllPostagens()
+  }
+
+  getAllTemas(){
+    this.TemaService.getAllTema().subscribe((resp: Tema[]) =>{ 
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.TemaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+  
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) =>{
+      this.listaPostagens = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert('Sua mensagem foi enviada amigo')
+      this.postagem = new Postagem()
+    })
   }
 
 }
